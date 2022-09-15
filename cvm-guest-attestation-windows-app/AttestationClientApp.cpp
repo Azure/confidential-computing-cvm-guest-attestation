9797b1ca8e2a90056f18f7faeef786f8490f887d
prasadmsft <reprasa@microsoft.com>
2022-09-14 18:04:16 -0700

    Updated MAA endpoint URL

--- cvm-guest-attestation-windows-app/AttestationClientApp.cpp
+++ cvm-guest-attestation-windows-app/AttestationClientApp.cpp
@@ -38,7 +38,7 @@ public:
 };
 
 // Attestation tenant URI + Guest attestation path + API version
-std::string attestation_url = "https://sharedeus.eus.test.attest.azure.net/attest/AzureGuest?api-version=2020-10-01";
+std::string attestation_url = "https://sharedeus2.eus2.attest.azure.net/attest/AzureGuest?api-version=2020-10-01";
 
 int main() {
     try {
