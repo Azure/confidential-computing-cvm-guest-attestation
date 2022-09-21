fdd86f188a7f09e8cd445da972b5c4251ec43888
Akash Gupta <akagu@microsoft.com>
2022-09-21 15:01:26 -0700

    Add the sample platform checker executables

--- aks-linux-sample/get-attestation-report.sh
+++ aks-linux-sample/get-attestation-report.sh
@@ -2,8 +2,7 @@
 
 /AttestationClient -o token>> /attestation_output
 
-OUTPUT=$(cat /attestation_output)
-JWT=$(echo -n $OUTPUT | rev | cut -d " " -f1 | rev)
+JWT=$(cat /attestation_output)
 
 echo -n $JWT | cut -d "." -f 1 | base64 -d 2>/dev/null | jq .
 echo -n $JWT | cut -d "." -f 2 | base64 -d 2>/dev/null | jq .
