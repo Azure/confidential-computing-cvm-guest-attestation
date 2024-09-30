5095c48d6d0cca2dde1c4b1bdd1b26be0c1c8741
Deepak Sharma <sharmade@microsoft.com>
2024-10-01 01:00:36 +0530

    Add one-click attestation

---
+++ cvm-attestation-sample-app/OneClickAttest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo ../client-library/src/Attestation/pre-requisites.sh
+sudo ../client-library/src/Attestation/build.sh
+sudo dpkg -i ../client-library/src/Attestation/_build/x86_64/packages/attestationlibrary/deb/azguestattestation1_1.0.5_amd64.deb
+sudo cmake .
+sudo make
+sudo ./AttestationClient -o token
