3759bd263441738b9e41873cb664e3042cfc7d03
Antoine Delignat-Lavaud <antdl@microsoft.com>
2024-11-05 21:56:44 +0000

    Expose stateful API with TPM decryption
    	modified:   client-library/src/Attestation/AttestationClient/lib/AttestationClient.cpp
    	modified:   client-library/src/Attestation/AttestationClient/lib/include/AttestationClient.h

--- client-library/src/Attestation/AttestationClient/lib/AttestationClient.cpp
+++ client-library/src/Attestation/AttestationClient/lib/AttestationClient.cpp
@@ -75,36 +75,90 @@ public:
   }
 };
 
-int32_t get_attestation_token(const uint8_t* app_data, uint32_t pcr, uint8_t* token, size_t* jwt_len, const char* endpoint_url) {
-    AttestationClient* attestation_client = nullptr;
+int32_t ga_create(void** st) {
     attest::AttestationLogger* logger = nullptr;
-    attest::ClientParameters params = {};
-    unsigned char* jwt = nullptr;
+
+    if(!st)
+        return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
 
     try {
         logger = new DefaultLogger();
         std::shared_ptr<attest::AttestationLogger> slogger(logger);
-        attestation_client = new AttestationClientImpl(slogger);
-
-        params.version = CLIENT_PARAMS_VERSION;
-        params.attestation_endpoint_url = (const unsigned char*)endpoint_url;
-        params.client_payload = app_data;
-        params.pcr_selector = pcr;
-
-        attest::AttestationResult::ErrorCode err = attestation_client->Attest(params, &jwt).code_;
-        if(err != attest::AttestationResult::ErrorCode::SUCCESS)
-          return (int32_t)err;
-
-        std::string jwt_str = reinterpret_cast<char*>(jwt);
-        *jwt_len = jwt_str.length();
-        if(*jwt_len >= 32*1024)
-          return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
-
-        std::strcpy((char*)token, (const char*)jwt_str.c_str());
-        attestation_client->Free(jwt);
+        *st = static_cast<void*>(new AttestationClientImpl(slogger));
         return 0;
     }
     catch (...) {
         return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
     }
 }
+
+void ga_free(void *st) {
+    if(!st) return;
+    delete static_cast<AttestationClient*>(st);
+}
+
+int32_t ga_get_token(void *st, const uint8_t* app_data, uint32_t pcr, uint8_t* token, size_t* jwt_len, const char* endpoint_url) {
+    if(!st) return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
+    AttestationClient* attestation_client = static_cast<AttestationClient*>(st);
+    unsigned char* jwt = nullptr;
+
+    attest::ClientParameters params = {
+        .version = CLIENT_PARAMS_VERSION,
+        .attestation_endpoint_url = (const unsigned char*)endpoint_url,
+        .client_payload = app_data,
+        .pcr_selector = pcr
+    };
+
+    try {
+        attest::AttestationResult::ErrorCode err = attestation_client->Attest(params, &jwt).code_;
+        if(err != attest::AttestationResult::ErrorCode::SUCCESS)
+          return (int32_t)err;
+
+        std::string jwt_str = reinterpret_cast<char*>(jwt);
+        *jwt_len = jwt_str.length();
+
+        if(*jwt_len >= 32*1024)
+          return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
+
+        std::strcpy((char*)token, (const char*)jwt_str.c_str());
+        attestation_client->Free(jwt);
+
+        return 0;
+    }
+    catch (...) {
+        return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
+    }
+}
+
+int32_t ga_decrypt(void *st, uint8_t *cipher, size_t* len) {
+    if(!st) return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
+    AttestationClient* attestation_client = static_cast<AttestationClient*>(st);
+    unsigned char *plaintext = NULL;
+    uint32_t plain_len = 0;
+
+    attest::AttestationResult res = attestation_client->Decrypt(attest::EncryptionType::NONE, cipher, *len, NULL, 0, &plaintext, &plain_len, attest::RsaScheme::RsaOaep, attest::RsaHashAlg::RsaSha256);
+    int32_t rc = (int32_t) res.code_;
+
+    if(rc) return rc;
+    if(!plaintext) return (int32_t)attest::AttestationResult::ErrorCode::ERROR_FAILED_MEMORY_ALLOCATION;
+
+    *len = plain_len;
+    memcpy(cipher, plaintext, plain_len);
+    attestation_client->Free(plaintext);
+
+    return 0;
+}
+
+int32_t get_attestation_token(const uint8_t* app_data, uint32_t pcr, uint8_t* token, size_t* jwt_len, const char* endpoint_url) {
+    void *st = NULL;
+    int32_t rc;
+
+    rc = ga_create(&st);
+
+    if(st)
+        rc = ga_get_token(st, app_data, pcr, token, jwt_len, endpoint_url);
+
+    ga_free(st);
+    return rc;
+}
+
